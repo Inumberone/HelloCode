1df68f2acacce4ead2a799dd7191b2bfd717dea0
Ivan <petiv12@yandex.ru>
2022-09-13 22:16:09 +0300

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter name:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ivan")
+{
+    Console.WriteLine("Ура, это же Ivan!");
+}
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
